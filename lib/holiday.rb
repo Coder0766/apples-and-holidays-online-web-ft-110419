b7ed15c45f9effc71d29cb0a221da4563ee46b53
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-12-07 03:26:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,6 +54,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter][:christmas] && [:new_years] =>  "supplies"
   # binding.pry
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
