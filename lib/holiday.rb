bca18cb15799fe20423d52d021faa659ddf6d866
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-12-07 05:45:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each_line do |line|
   # binding.pry
   end
-end  
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
