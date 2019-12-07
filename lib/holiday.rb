41f96c785bc4227c7f859c1cbc6f683197cac559
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-12-07 03:59:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   # holiday_hash[:winter].values.flatten
   # holiday_hash[:winter][:christmas]["Lights", "Wreath"] && [:new_years]["Party Hats"]
-    holiday_hash[:winter].each{|supply| yield supply}
+    holiday_hash[:winter].each{|supply| yield supplies}
   # binding.pry
 end
 
