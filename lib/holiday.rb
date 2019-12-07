6fd4ae4ebf01e915dbd8c33789fc6eb8461f40b0
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-12-07 04:16:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash[:season].collect {|key, value|}
+  holiday_hash[:season].each_pair {|key, value|}
   binding.pry
 end
 
