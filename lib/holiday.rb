b45291003b918612098ac1adaff5d90b4356bf53
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-12-07 05:44:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each {|holiday_hash|}
+  @output.each_line do |line|
   # binding.pry
 end
 
